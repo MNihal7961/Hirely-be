ada7e23ede7ce331c08fef7a4c7ab3f1c22008d6
MNihal7961 <mnihal7961@gmail.com>
2026-03-31 14:52:31 +0530

    app configuration updated

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -11,6 +12,11 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') ?? 3000;
 
+  app.use(cookieParser());
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
   await app.listen(port);
   Logger.log(`Application is running on port ${port}`, 'Bootstrap');
 }
