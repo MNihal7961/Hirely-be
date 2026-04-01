3d6b71b2b1a6b49ad524ddf0cddbe0609e6926bc
MNihal7961 <mnihal7961@gmail.com>
2026-04-01 13:44:17 +0530

    Add Mongoose integration for Interview entity in InterviewModule

--- src/interview/interview.module.ts
+++ src/interview/interview.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { InterviewService } from './interview.service';
 import { InterviewController } from './interview.controller';
 import { AiModelModule } from 'src/ai-model/ai-model.module';
+import { UserModule } from 'src/user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Interview, InterviewSchema } from './entities/interview.entity';
 
 @Module({
-  imports: [AiModelModule],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Interview.name, schema: InterviewSchema },
+    ]),
+    AiModelModule,
+    UserModule,
+  ],
   controllers: [InterviewController],
   providers: [InterviewService],
 })
